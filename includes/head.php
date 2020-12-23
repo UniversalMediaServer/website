4f35ea1408b2a58eb7d1a11570aedd3119aea36a
Kyle Martin <kyle.martin@hotmail.co.nz>
2020-12-23 18:53:57 +1300

    remove remixicon unused

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
-<meta content="" name="description">
-<meta content="" name="keywords">
+<meta name="description" content="Stream your media to your devices, whether they are TVs, smartphones, gaming consoles, computers, audio receivers, and more!">
+<meta name="keywords" content="UMS, Universal Media Server" >
 
 <!-- Favicons -->
 <link href="/assets/img/favicon.ico" rel="icon">
@@ -14,7 +14,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
 <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
 <link href="/assets/vendor/venobox/venobox.css" rel="stylesheet">
 <link href="/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
 <!-- <link href="/assets/vendor/aos/aos.css" rel="stylesheet"> -->
