29ab71e9204f4d325e61c5feefdbc03b5af3ad36
Klaus Townsend <subjunk@gmail.com>
2021-06-08 21:30:17 +1200

    Replace donate link with a page

---
+++ donate/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Universal Media Server | Donate</title>
+    <?php include '../includes/head.php'; ?>
+  </head>
+  <body>
+    <?php include '../includes/menu.php'; ?>
+    <section id="hero" class="d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" style="text-align: center;">
+            <h1>Donate</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <main id="main">
+      <section id="about" class="about section-bg">
+        <div class="container">
+          <div class="row content">
+            <div class="col-lg-12">
+              <h2>Patreon</h2>
+            </div>
+          </div>
+          <div class="row content">
+            <div class="col-lg-12">
+              <p>The best way to support the development of this project is to sponsor us on Patreon.</p>
+              <p>Your contribution goes towards offsetting the costs of our web servers - which serve over 240 million requests per month to 160,000 unique users - as well as provide reward and incentive to the development team to continue to improve our products.</p>
+              <p>There are also benefits available via Patreon depending on how much you decide to contribute. Thank you in advance for any contribution.</p>
+              <p><a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p>
+            </div>
+          </div>
+          <div class="row content">
+            <div class="col-lg-12">
+              <h2>PayPal</h2>
+              <p>If you would prefer to make a donation via PayPal, please use the following button:<br>
+              <a rel="nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZLD5J8DS6K6W"><img src="../images/btn_donateCC_LG.gif" border="0" /></a></p><br>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <?php include '../includes/footer.php'; ?>
+  </body>
+</html>
