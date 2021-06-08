48ebaf33357d068fb981fba2afa6b9e8234f4fba
Klaus Townsend <subjunk@gmail.com>
2021-06-08 21:34:57 +1200

    Formatting

--- donate/index.php
+++ donate/index.php
@@ -28,14 +28,14 @@
               <p>The best way to support the development of this project is to sponsor us on Patreon.</p>
               <p>Your contribution goes towards offsetting the costs of our web servers - which serve over 240 million requests per month to 160,000 unique users - as well as provide reward and incentive to the development team to continue to improve our products.</p>
               <p>There are also benefits available via Patreon depending on how much you decide to contribute. Thank you in advance for any contribution.</p>
-              <p><a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p>
+              <p>Donate to <a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p><br><br>
             </div>
           </div>
           <div class="row content">
             <div class="col-lg-12">
               <h2>PayPal</h2>
               <p>If you would prefer to make a donation via PayPal, please use the following button:<br>
-              <a rel="nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZLD5J8DS6K6W"><img src="../images/btn_donateCC_LG.gif" border="0" /></a></p><br>
+              <a rel="nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZLD5J8DS6K6W"><img src="../images/btn_donateCC_LG.gif" border="0" /></a></p>
             </div>
           </div>
         </div>
