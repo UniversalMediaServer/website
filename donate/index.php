0149c85cad1b84bfa690fa6382487393cbcf993b
Klaus Townsend <subjunk@gmail.com>
2021-08-24 21:08:23 +1200

    Formatting

--- donate/index.php
+++ donate/index.php
@@ -23,27 +23,27 @@
             <div class="col-lg-12">
               <p>Your contribution goes towards offsetting the costs of our web servers - which serve over 240 million requests per month to 160,000 unique users - as well as provide reward and incentive to the development team to continue to improve our products.</p>
               <p>We support 4 donation methods in an attempt to make it easier for you to use something you are familiar with. If you would like to donate using another method, please get in touch with us.</p>
-              <p><em>Thank</em> you in advance for any contribution.</p>
+              <p><em>Thank</em> you in advance for any contribution.</p><br>
             </div>
           </div>
           <div class="row content">
             <div class="col-lg-12">
               <h2>Patreon</h2>
               <p>There are benefits available via Patreon depending on how much you decide to contribute.</p>
-              <p>Donate to <a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p><br><br>
+              <p>Donate to <a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p><br>
             </div>
           </div>
           <div class="row content">
             <div class="col-lg-12">
               <h2>PayPal</h2>
-              <p>PayPal makes it easy to donate quickly via credit card with no account signup required. Please use the following button:<br>
-              <a rel="nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZLD5J8DS6K6W"><img src="../images/btn_donateCC_LG.gif" border="0" /></a></p><br><br>
+              <p>PayPal makes it easy to donate quickly via credit card with no account signup required. Please use the following button:<p>
+              <p><a rel="nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZLD5J8DS6K6W"><img src="../images/btn_donateCC_LG.gif" border="0" /></a></p><br>
             </div>
           </div>
           <div class="row content">
             <div class="col-lg-12">
               <h2>Open Collective</h2>
-              <p>Donate to <a href="https://opencollective.com/universalmediaserver">Universal Media Server on Open Collective</a>.</p><br><br>
+              <p>Donate to <a href="https://opencollective.com/universalmediaserver">Universal Media Server on Open Collective</a>.</p><br>
             </div>
           </div>
           <div class="row content">
@@ -51,7 +51,7 @@
               <h2>Cryptocurrency</h2>
               <p>We gladly accept donations in the following cryptocurrencies:</p>
               <p>Cardano (ADA): <code>addr1qx8g9nkln6zr4gs5f0r3ysnxgpqum4ekwnt5e77jaglzkensdvxfplz7lr8pg07kcvjpnrfcxzkht3hgmkr86vclmaxqjhja8c</code></p>
-              <p>If you would like to donate a cryptocurrency that is not on this page, please let us know and we will provide the details.</p><br><br>
+              <p>If you would like to donate a cryptocurrency that is not on this page, please let us know and we will provide the details.</p><br>
             </div>
           </div>
         </div>
