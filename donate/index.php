6f238dc83ebfaea4f52302ede042d4af1aa23b12
Klaus Townsend <subjunk@gmail.com>
2021-11-12 21:35:34 +1300

    Removed duplicate

--- donate/index.php
+++ donate/index.php
@@ -36,7 +36,7 @@
             <div class="col-lg-12">
               <p>To quickly donate, please use the button above, otherwise there are more options below.</p>
               <p>Your contribution goes towards offsetting the costs of our web servers - which serve over 240 million requests per month to 160,000 unique users - as well as provide reward and incentive to the development team to continue to improve our products.</p>
-              <p>We support 4 donation methods in an attempt to make it easier for you to use something you are familiar with. If you would like to donate using another method, please get in touch with us.</p>
+              <p>We support these donation methods in an attempt to make it easier for you to use something you are familiar with. If you would like to donate using another method, please get in touch with us.</p>
               <p><em>Thank</em> you in advance for any contribution.</p><br>
             </div>
           </div>
@@ -45,29 +45,6 @@
               <h2>Patreon</h2>
               <p>There are benefits available via Patreon depending on how much you decide to contribute.</p>
               <p>Donate to <a href="https://www.patreon.com/universalmediaserver">Universal Media Server on Patreon</a>.</p><br>
-            </div>
-          </div>
-          <div class="row content">
-            <div class="col-lg-12">
-              <h2>PayPal</h2>
-              <p>PayPal makes it easy to donate quickly via credit card with no account signup required. Please use the following button:<p>
-              <p>
-                <div id="donate-button-container">
-                  <div id="donate-button"></div>
-                  <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
-                  <script>
-                  PayPal.Donation.Button({
-                  env:'production',
-                  hosted_button_id:'KZLD5J8DS6K6W',
-                  image: {
-                  src:'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
-                  alt:'Donate with PayPal button',
-                  title:'PayPal - The safer, easier way to pay online!',
-                  }
-                  }).render('#donate-button');
-                  </script>
-                </div>
-              </p><br>
             </div>
           </div>
           <div class="row content">
