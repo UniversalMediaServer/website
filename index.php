f9e727f624d88d292daefcfbeee279a02289870d
Klaus Townsend <subjunk@gmail.com>
2021-12-24 09:06:38 +1300

    Styling

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                           </a>
                         </p>
                         <p>
-                          <a type="button" class="btn btn-info subscribe-button" href="https://www.patreon.com/bePatron?u=34420953">
+                          <a type="button" class="btn btn-danger subscribe-button" href="https://www.patreon.com/bePatron?u=34420953">
                             <svg viewBox="0 0 569 546" xmlns="http://www.w3.org/2000/svg" style="height:1rem;width:1rem;margin-right:0.5rem;"><g><circle cx="362.589996" cy="204.589996" data-fill="1" id="Oval" r="204.589996"></circle><rect data-fill="2" height="545.799988" id="Rectangle" width="100" x="0" y="0"></rect></g></svg>
                             Subscribe for the latest download: <strong><?php echo $umsVersionPatreon; ?></strong>
                           </a>
