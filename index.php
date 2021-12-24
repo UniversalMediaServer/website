c57560a21b1a1a9f950d58352493896bd227ec25
Klaus Townsend <subjunk@gmail.com>
2021-12-24 15:24:26 +1300

    Trigger download modal from menu too

--- index.php
+++ index.php
@@ -23,35 +23,6 @@
               <h1>Universal Media Server</h1>
               <h2>Stream your media to your devices, whether they are TVs, smartphones, gaming consoles, computers, audio receivers, and more!</h2>
               <div class="d-lg">
-                <div class="modal fade" id="downloadModal" tabindex="-1" aria-labelledby="downloadModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="downloadModalLabel">Download</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <p>
-                          <a type="button" class="btn btn-primary subscribe-button" href="https://www.patreon.com/bePatron?u=34420953">
-                            <svg viewBox="0 0 569 546" xmlns="http://www.w3.org/2000/svg"><g><circle cx="362.589996" cy="204.589996" data-fill="1" id="Oval" r="204.589996"></circle><rect data-fill="2" height="545.799988" id="Rectangle" width="100" x="0" y="0"></rect></g></svg>
-                            Subscribe for the latest download: <strong><?php echo $umsVersionPatreon; ?></strong>
-                          </a>
-                        </p>
-                        <p>Builds are released to <a href="https://www.patreon.com/bePatron?u=34420953" title="Universal Media Server on Patreon" target="_blank">Patreon</a> first to reward our patrons.</p>
-                        <p>An older, public build can be downloaded below:</p>
-                      </div>
-                      <div class="modal-footer">
-                        <a type="button" class="btn btn-light mr-auto" href="/download/">All Downloads</a>
-                        <a type="button" class="btn btn-secondary" href="https://www.fosshub.com/Universal-Media-Server.html?dwl=UMS-<?php echo $umsVersion; ?>.exe" id="download-link">Download <?php echo $umsVersion; ?></a>
-                        <?php if ($umsVersionBeta) { ?>
-                          <a href="https://www.fosshub.com/Universal-Media-Server.html?dwl=UMS-<?php echo $umsVersionBeta; ?>.exe" class="btn-download-beta scrollto" id="download-link-beta">Download beta</a>
-                        <?php } ?>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 <button class="btn-get-started scrollto" id="download-link" data-toggle="modal" data-target="#downloadModal"> Download Now <i class="icofont-download"></i></button>
               </div>
               <div class="ad-container">
