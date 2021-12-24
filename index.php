c49a75c8a5ed5d34f666d8b18da2a9ad4f79cc8e
Klaus Townsend <subjunk@gmail.com>
2021-12-24 13:45:21 +1300

    Button color

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                       </div>
                       <div class="modal-body">
                         <p>
-                          <a type="button" class="btn btn-danger subscribe-button" href="https://www.patreon.com/bePatron?u=34420953">
+                          <a type="button" class="btn btn-primary" href="https://www.patreon.com/bePatron?u=34420953">
                             <svg viewBox="0 0 569 546" xmlns="http://www.w3.org/2000/svg"><g><circle cx="362.589996" cy="204.589996" data-fill="1" id="Oval" r="204.589996"></circle><rect data-fill="2" height="545.799988" id="Rectangle" width="100" x="0" y="0"></rect></g></svg>
                             Subscribe for the latest download: <strong><?php echo $umsVersionPatreon; ?></strong>
                           </a>
@@ -41,7 +41,7 @@
                       </div>
                       <div class="modal-footer">
                         <a type="button" class="btn btn-light mr-auto" href="/download/">All Downloads</a>
-                        <a type="button" class="btn btn-primary" href="https://www.fosshub.com/Universal-Media-Server.html?dwl=UMS-<?php echo $umsVersion; ?>.exe" id="download-link">Download <?php echo $umsVersion; ?></a>
+                        <a type="button" class="btn btn-secondary" href="https://www.fosshub.com/Universal-Media-Server.html?dwl=UMS-<?php echo $umsVersion; ?>.exe" id="download-link">Download <?php echo $umsVersion; ?></a>
                         <?php if ($umsVersionBeta) { ?>
                           <a href="https://www.fosshub.com/Universal-Media-Server.html?dwl=UMS-<?php echo $umsVersionBeta; ?>.exe" class="btn-download-beta scrollto" id="download-link-beta">Download beta</a>
                         <?php } ?>
