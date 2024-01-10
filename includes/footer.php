c825643a27774731a3815e7daf5f2e9712d43b42
Klaus Townsend <subjunk@gmail.com>
2024-01-10 21:50:51 +1300

    Added social section to footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,18 @@
 
 <footer id="footer">
   <div class="container footer-bottom clearfix">
-    <div class="credits">
+    <div class="credits" style="margin-top: 26px;">
       <!-- All the links in the footer should remain intact. -->
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
       Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
     </div>
+    <span class="social" style="font-size: 48px;">
+      <a href="https://twitter.com/UMS16" title="UMS on Twitter"><i class="icofont-twitter"></i></a>
+      <a href="https://www.facebook.com/UniversalMediaServer" title="UMS on Facebook"><i class="icofont-facebook"></i></a>
+      <a href="https://github.com/UniversalMediaServer/UniversalMediaServer/" title="UMS on GitHub"><i class="icofont-github"></i></a>
+    </span>
   </div>
 </footer>
 
